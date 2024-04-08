7208bc55e2b81f4c5b43da5799adadb17d7d7829
dehero <dehero@outlook.com>
2024-04-09 00:24:16 +0300

    feat(site): implement Frame component

--- src/site/App.tsx
+++
@@ -1,6 +0,0 @@
-import type { Component } from 'solid-js';
-import styles from './App.module.css';
-
-export const App: Component = () => {
-  return <div class={styles.App}>Morrowind Screenshots</div>;
-};
---
+++ src/site/components/Frame/Frame.tsx
@@ -0,0 +1,13 @@
+import clsx from 'clsx';
+import type { Component, JSX } from 'solid-js';
+import styles from './Frame.module.css';
+
+interface FrameProps {
+  class?: string;
+  children?: JSX.Element;
+  variant: 'thin' | 'thick' | 'button';
+}
+
+export const Frame: Component<FrameProps> = ({ children, variant, ...props }) => {
+  return <div class={clsx(styles[variant], props.class)}>{children}</div>;
+};
