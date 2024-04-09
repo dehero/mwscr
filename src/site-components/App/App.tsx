32824c1d89c7615e027619b97a29fda52636f5a2
dehero <dehero@outlook.com>
2024-04-09 16:36:33 +0300

    feat(site): virtualize posts grid

--- src/site-components/App/App.tsx
+++ src/site-components/App/App.tsx
@@ -1,18 +1,81 @@
-import { type Component, For } from 'solid-js';
-import posts from '../../../data/published/2023.yml';
+import type { VirtualItemProps } from '@minht11/solid-virtual-container';
+import { VirtualContainer } from '@minht11/solid-virtual-container';
+import { type Component, createResource } from 'solid-js';
+import { comparePostEntriesById, type Post, type PostEntries } from '../../entities/post.js';
 import { Frame } from '../Frame/Frame.js';
 import { PostPreview } from '../PostPreview/PostPreview.js';
 import styles from './App.module.css';
 
-export const App: Component = () => {
+const postChunks = import.meta.glob('../../../data/published/*.yml', { import: 'default' });
+
+const getPosts = async (): Promise<PostEntries> => {
+  const result: Map<string, Post> = new Map();
+  const references: Map<string, string> = new Map();
+
+  for (const chunk of Object.values(postChunks)) {
+    const entries = Object.entries((await chunk()) as Record<string, Post | string>);
+    for (const [id, post] of entries) {
+      if (typeof post === 'string') {
+        references.set(post, id);
+      } else {
+        result.set(id, post);
+      }
+    }
+  }
+
+  // Resolve references
+  for (const [id, post] of references) {
+    const ref = result.get(post);
+    if (ref) {
+      result.set(id, ref);
+    }
+  }
+
+  return [...result.entries()].sort(comparePostEntriesById('desc'));
+};
+
+const ListItem: Component<VirtualItemProps<[string, Post | string]>> = (props) => {
   return (
-    <div class={styles.app}>
+    <div style={props.style} class={styles.listItem} tabIndex={props.tabIndex} role="listitem">
+      <PostPreview post={props.item[1]} />
+    </div>
+  );
+};
+
+const calculateGridItemSize = (crossAxisSize: number) => {
+  const maxWidth = 336;
+
+  const count = Math.ceil(crossAxisSize / maxWidth);
+  const width = Math.floor(crossAxisSize / count);
+
+  return {
+    width,
+    height: width + 34,
+  };
+};
+
+export const App: Component = () => {
+  let targetVertical;
+  const [posts] = createResource(getPosts);
+
+  return (
+    <>
       <Frame variant="thin" class={styles.frame}>
         Morrowind Screenshots
       </Frame>
-      <div class={styles.posts}>
-        <For each={Object.entries(posts)}>{([, post]) => <PostPreview post={post} />}</For>
+
+      <div ref={targetVertical} class={styles.scrollContainer}>
+        <VirtualContainer
+          items={posts()}
+          scrollTarget={targetVertical}
+          // Calculate how many grid columns to show.
+          crossAxisCount={(measurements) => Math.floor(measurements.container.cross / measurements.itemSize.cross)}
+          // overscan={10}
+          itemSize={calculateGridItemSize}
+        >
+          {ListItem}
+        </VirtualContainer>
       </div>
-    </div>
+    </>
   );
 };
