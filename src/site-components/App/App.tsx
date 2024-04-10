76fafc5cb40826a41169d98ac75160446ac3e835
dehero <dehero@outlook.com>
2024-04-10 09:45:39 +0300

    feat(site): implement RadioGroup and postType filter

--- src/site-components/App/App.tsx
+++ src/site-components/App/App.tsx
@@ -1,14 +1,20 @@
 import type { VirtualItemProps } from '@minht11/solid-virtual-container';
 import { VirtualContainer } from '@minht11/solid-virtual-container';
-import { type Component, createResource } from 'solid-js';
-import { comparePostEntriesById, type Post, type PostEntries } from '../../entities/post.js';
+import { type Component, createResource, createSignal } from 'solid-js';
+import type { Post, PostEntries, PostType } from '../../entities/post.js';
+import { comparePostEntriesById, POST_TYPES } from '../../entities/post.js';
 import { Button } from '../Button/Button.js';
 import { PostPreview } from '../PostPreview/PostPreview.js';
+import { RadioGroup } from '../RadioGroup/RadioGroup.jsx';
 import styles from './App.module.css';
 
 const postChunks = import.meta.glob('../../../data/published/*.yml', { import: 'default' });
 
-const getPosts = async (): Promise<PostEntries> => {
+interface PostsFilter {
+  type?: PostType;
+}
+
+const getPosts = async (filter: PostsFilter): Promise<PostEntries> => {
   const result: Map<string, Post> = new Map();
   const references: Map<string, string> = new Map();
 
@@ -31,7 +37,9 @@ const getPosts = async (): Promise<PostEntries> => {
     }
   }
 
-  return [...result.entries()].sort(comparePostEntriesById('desc'));
+  return [...result.entries()]
+    .filter(([, post]) => !filter.type || post.type === filter.type)
+    .sort(comparePostEntriesById('desc'));
 };
 
 const ListItem: Component<VirtualItemProps<[string, Post | string]>> = (props) => {
@@ -56,12 +64,24 @@ const calculateGridItemSize = (crossAxisSize: number) => {
 
 export const App: Component = () => {
   let targetVertical;
-  const [posts] = createResource(getPosts);
+  const [postType, setPostType] = createSignal<PostType | undefined>();
+
+  const postFilter = (): PostsFilter => ({
+    type: postType(),
+  });
+
+  const [posts] = createResource(postFilter, getPosts);
 
   return (
     <>
       <div class={styles.header}>
         <div class={styles.title}>Morrowind Screenshots</div>
+        <RadioGroup
+          options={[{ value: undefined, label: 'All' }, ...POST_TYPES.map((value) => ({ value }))]}
+          name="postType"
+          value={postType()}
+          onChange={setPostType}
+        />
         <Button
           // TODO: use github-issue-resolvers
           href="https://github.com/dehero/mwscr/issues/new?labels=proposal&template=proposal.yml"
---
+++ src/site-components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,39 @@
+import clsx from 'clsx';
+import { For } from 'solid-js';
+import { Frame } from '../Frame/Frame.js';
+import styles from './RadioGroup.module.css';
+
+interface RadioGroupOption<T> {
+  value: T | undefined;
+  label?: string;
+}
+
+interface RadioGroupProps<T> {
+  class?: string;
+  options: RadioGroupOption<T>[];
+  name: string;
+  onChange: (value: T | undefined) => void;
+  value?: T | undefined;
+}
+
+export function RadioGroup<T extends string>(props: RadioGroupProps<T>) {
+  return (
+    <div class={clsx(styles.group, props.class)}>
+      <For each={props.options}>
+        {(option) => (
+          <Frame component="label" variant="button" class={styles.option}>
+            <input
+              type="radio"
+              value={option.value ?? ''}
+              name={props.name}
+              checked={props.value === option.value}
+              onChange={(e) => props.onChange((e.target.value || undefined) as T | undefined)}
+              class={styles.radio}
+            />
+            <span class={styles.label}>{option.label || option.value}</span>
+          </Frame>
+        )}
+      </For>
+    </div>
+  );
+}
