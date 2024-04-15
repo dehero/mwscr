489ec8424b3146c8a52ba3569bed21c1a2f178b2
dehero <dehero@outlook.com>
2024-04-15 10:04:21 +0300

    fix: add preview maintenance step after resolving GitHub issue

--- src/resolve-github-issue.ts
+++ src/resolve-github-issue.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import { exchangeInboxAndTrash } from './functions/exchange-inbox-and-trash.js';
+import { maintainPreviews } from './functions/maintain-previews.js';
 import { renderDocs } from './functions/render-docs.js';
 import { resolveGithubIssue } from './functions/resolve-github-issue.js';
 
 await resolveGithubIssue();
 
 await exchangeInboxAndTrash();
+
+await maintainPreviews();
 
 await renderDocs();
