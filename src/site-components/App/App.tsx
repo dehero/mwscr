a8f96911e89e19ccdca7190edacb5aeccc3fbfa3
dehero <dehero@outlook.com>
2024-04-16 12:18:47 +0300

    feat(site): add header background

--- src/site-components/App/App.tsx
+++ src/site-components/App/App.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from '@solidjs/router';
 import { type Component, For, type JSX } from 'solid-js';
 import { Button } from '../Button/Button.js';
+import { Frame } from '../Frame/Frame.js';
 import { routes } from '../routes.js';
 import styles from './App.module.css';
 
@@ -13,20 +14,21 @@ export const App: Component<AppProps> = (props) => {
 
   return (
     <>
-      <header class={styles.header}>
-        <span class={styles.title}>
-          <img src="/icon.png" width={16} height={16} /> Morrowind Screenshots
-        </span>
+      <Frame variant="thick" component="header" class={styles.header}>
+        <div class={styles.title}>
+          <img src="/icon.png" width={16} height={16} class={styles.icon} />
+          Morrowind Screenshots
+        </div>
+      </Frame>
 
-        <nav class={styles.nav}>
-          <For each={routes}>
-            {(route) => (
-              <Button href={route.path} active={location.pathname === route.path}>
-                {route.info?.label ?? route.path}
-              </Button>
-            )}
-          </For>
-        </nav>
+      <nav class={styles.nav}>
+        <For each={routes}>
+          {(route) => (
+            <Button href={route.path} active={location.pathname === route.path}>
+              {route.info?.label ?? route.path}
+            </Button>
+          )}
+        </For>
 
         <Button
           // TODO: use github-issue-resolvers
@@ -35,7 +37,7 @@ export const App: Component<AppProps> = (props) => {
         >
           Propose
         </Button>
-      </header>
+      </nav>
 
       <main class={styles.main}>{props.children}</main>
     </>
