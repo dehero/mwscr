4d8a23701caad6cbf92cce7d10df0b55ec100d64
dehero <dehero@outlook.com>
2024-05-03 16:10:27 +0300

    fix(site): resolve users typing issues

--- src/site/data-managers/users.ts
+++ src/site/data-managers/users.ts
@@ -2,6 +2,9 @@ import users from '../../../data/users.yml';
 import type { User } from '../../core/entities/user.js';
 
 export function getUser(userId: string): User | undefined {
+  if (typeof users !== 'object' || Array.isArray(users)) {
+    return undefined;
+  }
   return users[userId] as User | undefined;
 }
 
