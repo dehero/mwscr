f95498a140a03ee372b506c806a649e09c751581
dehero <dehero@outlook.com>
2024-05-03 16:01:04 +0300

    feat(site): implement inbox and trash web posts managers

--- src/site/data-managers/posts.ts
+++ src/site/data-managers/posts.ts
@@ -1,8 +1,20 @@
-import { getPublishedPostChunkName } from '../../core/entities/post-variation.js';
+import { getPostDraftChunkName, getPublishedPostChunkName } from '../../core/entities/post-variation.js';
 import { WebPostsManager } from './utils/web-posts-manager.js';
 
 export const published = new WebPostsManager({
   name: 'published',
   chunksLoaders: import.meta.glob('../../../data/published/*.yml', { import: 'default' }),
   getPostChunkName: getPublishedPostChunkName,
+});
+
+export const inbox = new WebPostsManager({
+  name: 'inbox',
+  chunksLoaders: import.meta.glob('../../../data/inbox/*.yml', { import: 'default' }),
+  getPostChunkName: getPostDraftChunkName,
+});
+
+export const trash = new WebPostsManager({
+  name: 'trash',
+  chunksLoaders: import.meta.glob('../../../data/trash/*.yml', { import: 'default' }),
+  getPostChunkName: getPostDraftChunkName,
 });
