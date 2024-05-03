b4918e84669ee14db4c5f6f757cf243edb666cf9
dehero <dehero@outlook.com>
2024-05-03 16:05:38 +0300

    feat(site): implement PostComments and PostPublications components

--- src/core/utils/common-utils.ts
+++ src/core/utils/common-utils.ts
@@ -49,6 +49,22 @@ export function greatestCommonDivisor(a: number, b: number) {
   return greatestCommonDivisor(b, a % b);
 }
 
+export function groupBy<K, V>(list: Array<V>, keyGetter: (input: V) => K): Map<K, Array<V>> {
+  const map = new Map<K, Array<V>>();
+
+  list.forEach((item) => {
+    const key = keyGetter(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+
+  return map;
+}
+
 export function listItems(items: string[], quote?: boolean) {
   const parts: string[] = [];
 
