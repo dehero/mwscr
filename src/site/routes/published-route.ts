20d0b73e688a53230d81edf80abc6cea728fb5f0
dehero <dehero@outlook.com>
2024-05-06 11:54:53 +0300

    feat(site): use search params to store post filters

--- src/site/routes/published-route.ts
+++ src/site/routes/published-route.ts
@@ -1,5 +1,13 @@
-import type { SiteRoute } from '../../core/entities/site-route.js';
-import { PublishedPage } from '../pages/PublishedPage/PublishedPage.jsx';
+import type { SiteRoute, SiteRouteParams } from '../../core/entities/site-route.js';
+import { PublishedPage } from '../pages/PublishedPage/PublishedPage.js';
+
+export interface PublishedRouteParams extends SiteRouteParams {
+  type: string;
+  tag: string;
+  location: string;
+  author: string;
+  search: string;
+}
 
 export const publishedRoute: SiteRoute = {
   path: '/published/',
