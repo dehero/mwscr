7a0072f6f2e5d58455cf1636634d86636f50f33e
dehero <dehero@outlook.com>
2024-05-07 17:11:32 +0300

    feat(site): implement Dialog component

---
+++ src/site/components/Dialog/Dialog.tsx
@@ -0,0 +1,60 @@
+import clsx from 'clsx';
+import { type Component, createEffect, type JSX, onCleanup, Show } from 'solid-js';
+import { Portal } from 'solid-js/web';
+import frameStyles from '../Frame/Frame.module.css';
+import styles from './Dialog.module.css';
+
+export interface DialogProps {
+  class?: string;
+  children?: JSX.Element;
+  title?: string;
+  show: boolean;
+  onClose: () => void;
+  actions?: JSX.Element;
+}
+
+export const Dialog: Component<DialogProps> = (props) => {
+  let ref: HTMLDivElement | undefined;
+
+  const handleClick = (event: MouseEvent) => {
+    if (!ref?.contains(event.target as Node)) {
+      props.onClose();
+    }
+  };
+
+  createEffect(() => {
+    if (props.show) {
+      document.addEventListener('click', handleClick);
+
+      onCleanup(() => {
+        document.removeEventListener('click', handleClick);
+      });
+    }
+  });
+
+  return (
+    <Show when={props.show}>
+      <Portal>
+        <div class={styles.backdrop}>
+          <div ref={ref} class={clsx(props.title && frameStyles.thick, styles.container, props.class)}>
+            <Show when={props.title}>
+              <div class={styles.header}>{props.title}</div>
+            </Show>
+            <div class={clsx(frameStyles.thick, styles.body)}>
+              <div class={styles.content}>{props.children}</div>
+              <Show when={props.actions}>
+                <div class={styles.footer}>
+                  <div class={styles.spacer} />
+                  {props.actions}
+                  <Show when={!props.title}>
+                    <div class={styles.spacer} />
+                  </Show>
+                </div>
+              </Show>
+            </div>
+          </div>
+        </div>
+      </Portal>
+    </Show>
+  );
+};
