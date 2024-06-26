de29fbf580727d3a3a0c4bda63148c5b94c3f0e5
dehero <dehero@outlook.com>
2024-06-26 13:15:43 +0300

    feat(site): collapse navigation buttons to menu on narrow screen

---
+++ src/site/components/Navigation/Navigation.tsx
@@ -0,0 +1,66 @@
+import { type Component, For } from 'solid-js';
+import { navigate } from 'vike/client/router';
+import { usePageContext } from 'vike-solid/usePageContext';
+import { helpRoute } from '../../routes/help-route.js';
+import { homeRoute } from '../../routes/home-route.js';
+import { postsRoute } from '../../routes/posts-route.js';
+import { usersRoute } from '../../routes/users-route.js';
+import { Button } from '../Button/Button.jsx';
+import type { SelectOption } from '../Select/Select.jsx';
+import { Select } from '../Select/Select.jsx';
+import styles from './Navigation.module.css';
+
+const navigationItems = [
+  { route: homeRoute },
+  { route: postsRoute, params: { managerName: 'published' } },
+  { route: usersRoute },
+  { route: helpRoute, params: { topicId: '' } },
+  { route: postsRoute, params: { managerName: 'inbox' } },
+  { route: postsRoute, params: { managerName: 'trash' } },
+];
+
+type NavigationItem = (typeof navigationItems)[number];
+
+export function createOption({ route, params }: NavigationItem): SelectOption<string> {
+  const info = route.info(params as never, undefined as never);
+  const url = route.createUrl(params as never);
+
+  return {
+    label: info.label || info.title,
+    value: url,
+  };
+}
+
+export const Navigation: Component = () => {
+  const location = usePageContext().urlParsed;
+
+  const options = () => navigationItems.map(createOption);
+  const selectedOption = () =>
+    options().find((option) =>
+      location.pathname === '/'
+        ? option.value === location.pathname
+        : option.value && option.value !== '/'
+          ? location.pathname.startsWith(option.value)
+          : undefined,
+    );
+
+  return (
+    <nav class={styles.nav}>
+      <For each={options()}>
+        {(option) => (
+          <Button href={option.value} active={option.value === selectedOption()?.value} class={styles.button}>
+            {option.label}
+          </Button>
+        )}
+      </For>
+
+      <Select
+        options={options()}
+        // @ts-expect-error No proper typing for navigate
+        onChange={(value) => navigate(value)}
+        value={selectedOption()?.value}
+        class={styles.menu}
+      />
+    </nav>
+  );
+};
--- src/site/components/Page/Page.tsx
+++ src/site/components/Page/Page.tsx
@@ -1,16 +1,9 @@
 import './Page.css';
-import clsx from 'clsx';
 import { type Component, type JSX } from 'solid-js';
 import { usePageContext } from 'vike-solid/usePageContext';
 import { useRouteInfo } from '../../hooks/useRouteInfo.js';
-import { helpRoute } from '../../routes/help-route.js';
-import { homeRoute } from '../../routes/home-route.js';
-import { postRoute } from '../../routes/post-route.js';
-import { postsRoute } from '../../routes/posts-route.js';
-import { userRoute } from '../../routes/user-route.js';
-import { usersRoute } from '../../routes/users-route.js';
 import { Frame } from '../Frame/Frame.js';
-import { RouteButton } from '../RouteButton/RouteButton.js';
+import { Navigation } from '../Navigation/Navigation.js';
 import { Toaster } from '../Toaster/Toaster.js';
 import styles from './Page.module.css';
 import { YandexMetrikaCounter } from './YandexMetrikaCounter.js';
@@ -31,35 +24,7 @@ export const Page: Component<PageProps> = (props) => {
           <h1 class={styles.title}>{info()?.title || 'Morrowind Screenshots'}</h1>
         </Frame>
 
-        <nav class={styles.nav}>
-          <RouteButton route={homeRoute} />
-
-          <RouteButton
-            route={postsRoute}
-            activeRoutes={[postRoute, postsRoute]}
-            params={{ managerName: 'published' }}
-            matchParams
-          />
-
-          <RouteButton route={usersRoute} activeRoutes={[userRoute, usersRoute]} />
-
-          <RouteButton route={helpRoute} params={{ topicId: '' }} />
-
-          <RouteButton
-            route={postsRoute}
-            activeRoutes={[postRoute, postsRoute]}
-            params={{ managerName: 'inbox' }}
-            matchParams
-            class={clsx(styles.pullRight, styles.navItem)}
-          />
-
-          <RouteButton
-            route={postsRoute}
-            activeRoutes={[postRoute, postsRoute]}
-            params={{ managerName: 'trash' }}
-            matchParams
-          />
-        </nav>
+        <Navigation />
 
         {props.children}
       </Toaster>
