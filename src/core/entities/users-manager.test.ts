e6cc2ed03f5a2e6aeba951e72716de0a7ba82742
dehero <dehero@outlook.com>
2024-09-18 21:51:56 +0300

    test: add user ID creating tests

---
+++ src/core/entities/users-manager.test.ts
@@ -0,0 +1,52 @@
+import assert from 'node:assert';
+import test from 'node:test';
+import type { User } from './user.js';
+import { UsersManager } from './users-manager.js';
+
+class TestUsersManager extends UsersManager {
+  public createItemId(item: User) {
+    return super.createItemId(item);
+  }
+
+  public async saveChunk(_chunkName: string): Promise<void> {}
+
+  public async loadChunkData(_chunkName: string): Promise<Array<[string, User | string]>> {
+    return [];
+  }
+}
+
+test('UsersManager.createItemId', async (t) => {
+  const manager = new TestUsersManager();
+
+  await t.test('should generate ID based on user name', () => {
+    const user: User = {
+      name: 'John Doe',
+      profiles: {},
+    };
+    const id = manager.createItemId(user);
+    assert.strictEqual(id, 'john-doe');
+  });
+
+  await t.test('should generate ID based on first profile name', () => {
+    const user: User = {
+      name: '         ',
+      profiles: {
+        ig: 'john_doe',
+      },
+    };
+    const id = manager.createItemId(user);
+    assert.strictEqual(id, 'john-doe');
+  });
+
+  await t.test('should generate ID based on first profile name (when multiple)', () => {
+    const user: User = {
+      name: '',
+      profiles: {
+        ig: 'john_doe',
+        tg: 'john_doe2',
+      },
+    };
+    const id = manager.createItemId(user);
+    assert.strictEqual(id, 'john-doe');
+  });
+});
