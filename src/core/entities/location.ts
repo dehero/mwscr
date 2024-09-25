dedb0a668005693a00000164967e14f3430d2df5
dehero <dehero@outlook.com>
2024-09-25 08:30:11 +0300

    feat(map): extend LocationInfo

--- src/core/entities/location.ts
+++ src/core/entities/location.ts
@@ -1,6 +1,9 @@
+import { asArray } from '../utils/common-utils.js';
 import type { PostAddon } from './post.js';
 
 export type LocationCell = `${number} ${number}`;
+
+export type LocationCellCoordinates = [number, number];
 
 export const LOCATION_TYPES = ['interior', 'exterior', 'region', 'virtual'] as const;
 
@@ -22,6 +25,26 @@ export function isNestedLocation(location1: string, location2: string) {
   return location1.startsWith(location2);
 }
 
-export function getLocationCellCoordinates(cell: string): [number, number] {
-  return cell.split(' ').map(Number) as [number, number];
+export function getLocationCellCoordinates(cell: string): LocationCellCoordinates {
+  return cell.split(' ').map(Number) as LocationCellCoordinates;
+}
+
+export function locationCellsToString(cell: LocationCell | LocationCell[] | undefined): string {
+  return asArray(cell).join(' ');
+}
+
+export function stringToLocationCells(cells: string | undefined): LocationCell[] {
+  return (
+    cells
+      ?.split(' ')
+      .map((item) => Number(item) || 0)
+      .reduce(
+        (result, _, index, array) => {
+          if (index % 2 === 0) result.push(array.slice(index, index + 2));
+          return result;
+        },
+        [] as Array<number[]>,
+      )
+      .map(([x, y]) => `${x} ${y}` as LocationCell) ?? []
+  );
 }
