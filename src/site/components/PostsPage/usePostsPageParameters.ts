095d7d3e5172b63cbbe88f6abb1b6f4ddc26b97d
dehero <dehero@outlook.com>
2024-09-30 10:21:58 +0300

    feat(map): add Locations and Map tabs to posts selection parameters

---
+++ src/site/components/PostsPage/usePostsPageParameters.ts
@@ -0,0 +1,151 @@
+import { ANY_OPTION, NONE_OPTION, Option } from '../../../core/entities/option.js';
+import type { PostMark, PostType } from '../../../core/entities/post.js';
+import { POST_MARKS, POST_TYPES, POST_VIOLATIONS } from '../../../core/entities/post.js';
+import type { SelectPostInfosParams, SelectPostInfosSortKey } from '../../../core/entities/post-info.js';
+import { selectPostInfosSortOptions } from '../../../core/entities/post-info.js';
+import type { DateRange, SortDirection } from '../../../core/utils/common-types.js';
+import { isObjectEqual, stringToBool } from '../../../core/utils/common-utils.js';
+import { dateRangeToString, stringToDateRange } from '../../../core/utils/date-utils.js';
+import { useSearchParams } from '../../hooks/useSearchParams.js';
+import { PostsPageInfo, PostsPageSearchParams } from './PostsPage.jsx';
+import { useParams } from '../../hooks/useParams.js';
+import type { PostsRouteParams } from '../../routes/posts-route.js';
+import { Accessor } from 'solid-js';
+
+const emptySearchParams: PostsPageSearchParams = {
+  type: undefined,
+  tag: undefined,
+  location: undefined,
+  author: undefined,
+  requester: undefined,
+  mark: undefined,
+  violation: undefined,
+  publishable: undefined,
+  original: undefined,
+  search: undefined,
+  sort: undefined,
+  date: undefined,
+};
+
+interface PostsPagePreset extends Option {
+  searchParams: PostsPageSearchParams;
+}
+
+const presets = [
+  { value: undefined, label: 'All', searchParams: {} },
+  {
+    value: 'editors-choice',
+    label: "Editor's Choice",
+    searchParams: { sort: 'mark,desc', original: 'true' },
+  },
+  { value: 'shortlist', label: 'Shortlist', searchParams: { publishable: 'true' } },
+  { value: 'requests', label: 'Requests', searchParams: { requester: 'any', original: 'true' } },
+  { value: 'revisit', label: 'Revisit', searchParams: { mark: 'F' } },
+  {
+    value: 'unlocated',
+    label: 'Unlocated',
+    searchParams: { location: NONE_OPTION.value, original: 'true' },
+  },
+  { value: 'violations', label: 'Violations', searchParams: { violation: ANY_OPTION.value } },
+] as const satisfies PostsPagePreset[];
+
+export type PresetKey = (typeof presets)[number]['value'];
+
+export type FilterKey = keyof Pick<
+  PostsPageSearchParams,
+  'type' | 'tag' | 'location' | 'author' | 'mark' | 'violation' | 'publishable' | 'original' | 'requester' | 'date'
+>;
+
+export function usePostsPageParameters(info: Accessor<PostsPageInfo | undefined>) {
+  const routeParams = useParams<PostsRouteParams>();
+  const [searchParams, setSearchParams] = useSearchParams<PostsPageSearchParams>();
+
+  const sortOptions = () =>
+    selectPostInfosSortOptions.filter((item) => !info()?.sortKeys || info()?.sortKeys?.includes(item.value));
+  const presetOptions = (): PostsPagePreset[] => {
+    const options: PostsPagePreset[] = presets.filter(
+      (item) => !item.value || !info()?.presetKeys || info()?.presetKeys?.includes(item.value),
+    );
+    const currentPreset = options.find((preset) => isObjectEqual(preset.searchParams, searchParams));
+
+    if (!currentPreset) {
+      options.push({ value: 'custom', label: 'Custom Selection', searchParams });
+    }
+
+    return options;
+  };
+
+  const original = () => stringToBool(searchParams.original);
+  const publishable = () => stringToBool(searchParams.publishable);
+  const type = () => POST_TYPES.find((info) => info.id === searchParams.type)?.id;
+  const tag = () => searchParams.tag;
+  const location = () => searchParams.location;
+  const author = () => searchParams.author;
+  const requester = () => searchParams.requester;
+  const mark = () => POST_MARKS.find((info) => info.id === searchParams.mark)?.id;
+  const violation = () =>
+    [ANY_OPTION.value, NONE_OPTION.value, ...Object.keys(POST_VIOLATIONS)].find(
+      (violation) => violation === searchParams.violation,
+    ) as SelectPostInfosParams['violation'];
+  const sortKey = () =>
+    sortOptions().find((sortOption) => sortOption.value === searchParams.sort?.split(',')[0])?.value || 'date';
+  const sortDirection = () => (searchParams.sort?.split(',')[1] === 'asc' ? 'asc' : 'desc');
+  const search = () => searchParams.search;
+  const preset = () => presetOptions().find((preset) => isObjectEqual(preset.searchParams, searchParams))?.value;
+  const date = (): DateRange | undefined => (searchParams.date ? stringToDateRange(searchParams.date) : undefined);
+
+  const setPreset = (preset: string | undefined) =>
+    setSearchParams({ ...emptySearchParams, ...presetOptions().find((item) => item.value === preset)?.searchParams });
+  const setOriginal = (original: boolean | undefined) => setSearchParams({ original });
+  const setPublishable = (publishable: boolean | undefined) => setSearchParams({ publishable });
+  const setType = (type: PostType | undefined) => setSearchParams({ type });
+  const setTag = (tag: string | undefined) => setSearchParams({ tag });
+  const setLocation = (location: string | undefined) =>
+    setSearchParams({ location, original: Boolean(location) || undefined });
+  const setAuthor = (author: string | undefined) => setSearchParams({ author });
+  const setRequester = (requester: string | undefined) => setSearchParams({ requester });
+  const setMark = (mark: PostMark | undefined) => setSearchParams({ mark });
+  const setViolation = (violation: SelectPostInfosParams['violation'] | undefined) => setSearchParams({ violation });
+  const setSearch = (search: string | undefined) => setSearchParams({ search });
+  const setSortKey = (key: SelectPostInfosSortKey | undefined) =>
+    setSearchParams({ sort: `${key || sortKey()},${sortDirection()}` });
+  const setSortDirection = (direction: SortDirection | undefined) =>
+    setSearchParams({ sort: `${sortKey()},${direction || sortDirection()}` });
+  const setDate = (date: DateRange | undefined) => {
+    setSearchParams({ date: date ? dateRangeToString(date) : undefined });
+  };
+
+  return {
+    sortOptions,
+    presetOptions,
+    routeParams,
+    preset,
+    original,
+    publishable,
+    type,
+    tag,
+    location,
+    author,
+    requester,
+    mark,
+    violation,
+    sortKey,
+    sortDirection,
+    search,
+    date,
+    setPreset,
+    setOriginal,
+    setPublishable,
+    setType,
+    setTag,
+    setLocation,
+    setAuthor,
+    setRequester,
+    setMark,
+    setViolation,
+    setSearch,
+    setSortKey,
+    setSortDirection,
+    setDate,
+  };
+}
--- src/site/pages/posts/+data.ts
+++ src/site/pages/posts/+data.ts
@@ -1,24 +1,25 @@
 import type { PageContext } from 'vike/types';
+import { POSTS_MANAGER_INFOS } from '../../../core/entities/posts-manager.js';
 import { localDataExtractor } from '../../../local/data-managers/extractor.js';
 import type { PostsPageData } from '../../components/PostsPage/PostsPage.js';
 
 export async function data(pageContext: PageContext): Promise<PostsPageData> {
-  const managerName = pageContext.routeParams?.managerName;
+  const managerName = POSTS_MANAGER_INFOS.find((info) => info.id === pageContext.routeParams?.managerName)?.id;
   if (!managerName) {
-    return { postInfos: [], authorOptions: [], requesterOptions: [], locationOptions: [], tagOptions: [] };
+    return { postInfos: [], authorOptions: [], requesterOptions: [], locationInfos: [], tagOptions: [] };
   }
 
   const postInfos = await localDataExtractor.getAllPostInfos(managerName);
   const authorOptions = await localDataExtractor.getAuthorOptions(managerName);
   const requesterOptions = await localDataExtractor.getRequesterOptions(managerName);
-  const locationOptions = await localDataExtractor.getLocationOptions(managerName);
+  const locationInfos = await localDataExtractor.getAllLocationInfos();
   const tagOptions = await localDataExtractor.getTagOptions(managerName);
 
   return {
     postInfos,
     authorOptions,
     requesterOptions,
-    locationOptions,
+    locationInfos: locationInfos.filter((info) => info.discovered?.[managerName]),
     tagOptions,
   };
 }
