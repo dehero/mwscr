d61d557f76523e8687e1fd806b6f76f763af372e
dehero <dehero@outlook.com>
2024-12-04 22:51:04 +0300

    perf: evaluate dynamic data extraction

--- src/site/data-managers/extractor.ts
+++ src/site/data-managers/extractor.ts
@@ -1,35 +1,33 @@
 import { DataExtractor } from '../../core/entities/data-extractor.js';
-import type { LocationInfo } from '../../core/entities/location-info.js';
-import type { PostInfo } from '../../core/entities/post-info.js';
-import type { PostsManagerName } from '../../core/entities/posts-manager.js';
-import type { UserInfo } from '../../core/entities/user-info.js';
+// import type { LocationInfo } from '../../core/entities/location-info.js';
+// import type { PostInfo } from '../../core/entities/post-info.js';
+// import type { PostsManagerName } from '../../core/entities/posts-manager.js';
+// import type { UserInfo } from '../../core/entities/user-info.js';
 import { locations } from './locations.js';
 import { postsManagers } from './posts.js';
 import { users } from './users.js';
 
 class SiteDataExtractor extends DataExtractor {
-  async getAllLocationInfos(): Promise<LocationInfo[]> {
-    return (await import('virtual:locationInfos')).default;
-  }
-
-  async getAllPostInfos(managerName: PostsManagerName): Promise<PostInfo[]> {
-    // Cannot use `virtual:postInfos?managerName=${managerName}`
-    // See https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations
-    switch (managerName) {
-      case 'posts':
-        return (await import('virtual:postInfos?managerName=posts')).default;
-      case 'inbox':
-        return (await import('virtual:postInfos?managerName=inbox')).default;
-      case 'trash':
-        return (await import('virtual:postInfos?managerName=trash')).default;
-      default:
-        throw new Error(`Cannot find posts manager "${managerName}"`);
-    }
-  }
-
-  async getAllUserInfos(): Promise<UserInfo[]> {
-    return (await import('virtual:userInfos')).default;
-  }
+  // async getAllLocationInfos(): Promise<LocationInfo[]> {
+  //   return (await import('virtual:locationInfos')).default;
+  // }
+  // async getAllPostInfos(managerName: PostsManagerName): Promise<PostInfo[]> {
+  //   // Cannot use `virtual:postInfos?managerName=${managerName}`
+  //   // See https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations
+  //   switch (managerName) {
+  //     case 'posts':
+  //       return (await import('virtual:postInfos?managerName=posts')).default;
+  //     case 'inbox':
+  //       return (await import('virtual:postInfos?managerName=inbox')).default;
+  //     case 'trash':
+  //       return (await import('virtual:postInfos?managerName=trash')).default;
+  //     default:
+  //       throw new Error(`Cannot find posts manager "${managerName}"`);
+  //   }
+  // }
+  // async getAllUserInfos(): Promise<UserInfo[]> {
+  //   return (await import('virtual:userInfos')).default;
+  // }
 }
 
 export const siteDataExtractor = new SiteDataExtractor({
