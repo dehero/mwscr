a6362f02d6a19676211de6c2e74b762bbf2f5a31
dehero <dehero@outlook.com>
2024-12-10 11:07:44 +0300

    fix: don't use UserInfo in post page pre-rendered data

--- src/site/components/UserTooltip/UserTooltip.tsx
+++ src/site/components/UserTooltip/UserTooltip.tsx
@@ -1,6 +1,7 @@
-import { type Component, Show, splitProps } from 'solid-js';
+import { type Component, createResource, Show, splitProps } from 'solid-js';
 import { postsUsageToString } from '../../../core/entities/posts-usage.js';
 import type { UserInfo } from '../../../core/entities/user-info.js';
+import { dataExtractor } from '../../data-managers/extractor.js';
 import { GoldIcon } from '../GoldIcon/GoldIcon.js';
 import { Icon } from '../Icon/Icon.js';
 import type { TooltipProps } from '../Tooltip/Tooltip.js';
@@ -8,67 +9,76 @@ import { Tooltip } from '../Tooltip/Tooltip.js';
 import styles from './UserTooltip.module.css';
 
 interface UserTooltipProps extends Omit<TooltipProps, 'children'> {
-  userInfo: UserInfo;
+  user: UserInfo | string | undefined;
 }
 
 export const UserTooltip: Component<UserTooltipProps> = (props) => {
-  const [local, rest] = splitProps(props, ['userInfo']);
-  const authored = () => postsUsageToString(local.userInfo.authored);
-  const requested = () => postsUsageToString(local.userInfo.requested);
+  const [local, rest] = splitProps(props, ['user']);
+
+  const [userInfo] = createResource(local.user, (user) =>
+    typeof user === 'string' ? dataExtractor.getUserInfo(user) : user,
+  );
+
+  const authored = () => postsUsageToString(userInfo()?.authored);
+  const requested = () => postsUsageToString(userInfo()?.requested);
 
   return (
-    <Tooltip {...rest}>
-      <span class={styles.title}>
-        <Icon
-          size="small"
-          variant="flat"
-          color={local.userInfo.roles.includes('author') ? 'stealth' : 'magic'}
-          class={styles.icon}
-        >
-          {local.userInfo.title[0]?.toLocaleUpperCase() || '?'}
-        </Icon>
-        {local.userInfo.title}
-      </span>
+    <Show when={userInfo()}>
+      {(userInfo) => (
+        <Tooltip {...rest}>
+          <span class={styles.title}>
+            <Icon
+              size="small"
+              variant="flat"
+              color={userInfo().roles.includes('author') ? 'stealth' : 'magic'}
+              class={styles.icon}
+            >
+              {userInfo().title[0]?.toLocaleUpperCase() || '?'}
+            </Icon>
+            {userInfo().title}
+          </span>
 
-      <Show when={local.userInfo.roles.length > 0}>
-        <span class={styles.roles}>Roles: {local.userInfo.roles.join(', ')}</span>
-      </Show>
-      <Show when={authored()}>
-        <span class={styles.contribution}>
-          {'Authored: '}
-          <Show when={local.userInfo.authored?.posts}>
-            <GoldIcon class={styles.icon} />
+          <Show when={userInfo().roles.length > 0}>
+            <span class={styles.roles}>Roles: {userInfo().roles.join(', ')}</span>
           </Show>
-          {authored()}
-        </span>
-      </Show>
-      <Show when={requested()}>
-        <span>Requested: {requested()}</span>
-      </Show>
-      <Show when={local.userInfo.mark}>
-        <span class={styles.mark}>
-          {"Editor's Mark: "}
-          <Icon color="combat" size="small" variant="flat" class={styles.icon}>
-            {props.userInfo.mark?.[0]}
-          </Icon>
-          {local.userInfo.mark?.[1]}
-        </span>
-      </Show>
-      <Show when={local.userInfo.rating > 0}>
-        <span>Rating: {local.userInfo.rating}</span>
-      </Show>
-      <Show when={local.userInfo.likes > 0}>
-        <span>Likes: {local.userInfo.likes}</span>
-      </Show>
-      <Show when={local.userInfo.views > 0}>
-        <span>Views: {local.userInfo.views}</span>
-      </Show>
-      <Show when={local.userInfo.engagement > 0}>
-        <span>Engagement: {local.userInfo.engagement}</span>
-      </Show>
-      <Show when={local.userInfo.talkedToTelegramBot}>
-        <span class={styles.talkedToTelegramBot}>Talked to Ordinator</span>
-      </Show>
-    </Tooltip>
+          <Show when={authored()}>
+            <span class={styles.contribution}>
+              {'Authored: '}
+              <Show when={userInfo().authored?.posts}>
+                <GoldIcon class={styles.icon} />
+              </Show>
+              {authored()}
+            </span>
+          </Show>
+          <Show when={requested()}>
+            <span>Requested: {requested()}</span>
+          </Show>
+          <Show when={userInfo().mark}>
+            <span class={styles.mark}>
+              {"Editor's Mark: "}
+              <Icon color="combat" size="small" variant="flat" class={styles.icon}>
+                {userInfo().mark?.[0]}
+              </Icon>
+              {userInfo().mark?.[1]}
+            </span>
+          </Show>
+          <Show when={userInfo().rating > 0}>
+            <span>Rating: {userInfo().rating}</span>
+          </Show>
+          <Show when={userInfo().likes > 0}>
+            <span>Likes: {userInfo().likes}</span>
+          </Show>
+          <Show when={userInfo().views > 0}>
+            <span>Views: {userInfo().views}</span>
+          </Show>
+          <Show when={userInfo().engagement > 0}>
+            <span>Engagement: {userInfo().engagement}</span>
+          </Show>
+          <Show when={userInfo().talkedToTelegramBot}>
+            <span class={styles.talkedToTelegramBot}>Talked to Ordinator</span>
+          </Show>
+        </Tooltip>
+      )}
+    </Show>
   );
 };
