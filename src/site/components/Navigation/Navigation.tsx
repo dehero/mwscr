34b76c79aa8dd800d636178caa88faa1ef5c8265
dehero <dehero@outlook.com>
2025-02-04 09:31:57 +0300

    feat(site): implement OverflowContainer for Navigation menu

--- src/site/components/Navigation/Navigation.tsx
+++ src/site/components/Navigation/Navigation.tsx
@@ -10,8 +10,8 @@ import { resolveFirstRoute } from '../../routes/index.js';
 import { postsRoute } from '../../routes/posts-route.js';
 import { usersRoute } from '../../routes/users-route.js';
 import { Button } from '../Button/Button.js';
+import { OverflowContainer } from '../OverflowContainer/OverflowContainer.jsx';
 import { Select } from '../Select/Select.js';
-import { Spacer } from '../Spacer/Spacer.js';
 import styles from './Navigation.module.css';
 
 const navigationItems = [
@@ -71,24 +71,28 @@ export const Navigation: Component = () => {
 
   return (
     <nav class={styles.nav}>
-      <For each={options()}>
-        {(option) => (
-          <Button href={option.value} active={option.value === selectedOption()?.value} class={styles.button}>
-            {option.label}
-          </Button>
-        )}
-      </For>
-
-      <Select
-        options={options()}
-        // @ts-expect-error No proper typing for navigate
-        onChange={(value) => navigate(value)}
-        value={selectedOption()?.value}
+      <OverflowContainer
+        fallback={
+          <Select
+            options={options()}
+            // @ts-expect-error No proper typing for navigate
+            onChange={(value) => navigate(value)}
+            value={selectedOption()?.value}
+          />
+        }
+        containerClass={styles.menuWrapper}
         class={styles.menu}
-      />
+      >
+        <For each={options()}>
+          {(option) => (
+            <Button href={option.value} active={option.value === selectedOption()?.value}>
+              {option.label}
+            </Button>
+          )}
+        </For>
+      </OverflowContainer>
 
       <Show when={breadcrumbs().length > 1}>
-        <Spacer />
         <span class={styles.breadcrumbs}>
           <For each={breadcrumbs()}>
             {(breadcrumb, index) => (
