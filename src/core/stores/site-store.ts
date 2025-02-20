56ca70240d36118f9fff8ceb17d8ec717dae145a
dehero <dehero@outlook.com>
2025-02-20 16:47:46 +0300

    feat(site): implement playing clips

--- src/core/stores/site-store.ts
+++ src/core/stores/site-store.ts
@@ -2,7 +2,7 @@ import type { Store } from '../entities/store.js';
 import { site } from '../services/site.js';
 
 export class SiteStore implements Store {
-  include = ['shots/*.png', 'drawings/*.png', 'wallpapers/*.png'];
+  include = ['shots/*.png', 'drawings/*.png', 'wallpapers/*.png', 'videos/*.mp4'];
 
   getPublicUrl(path: string): string | undefined {
     return `${site.origin}/store/${path}`;
