21e11d30baf27baeeef1aad6f3cbec8273aa3cf0
dehero <dehero@outlook.com>
2025-03-07 15:59:22 +0300

    feat(site): implement file uploading

--- src/site/data-managers/resources.ts
+++ src/site/data-managers/resources.ts
@@ -1,3 +1,5 @@
 export function getResourcePreviewUrl(url: string | undefined) {
-  return url?.replace(/^store:\/(.*)\..*/, '/previews/$1.avif');
+  return url
+    ?.replace(/^store:\/(.*)\..*/, '/previews/$1.avif')
+    .replace(/^https\:\/\/mwscr\.dehero\.site\/uploads\/(.*)\..*/, '/uploads/$1.webp');
 }
