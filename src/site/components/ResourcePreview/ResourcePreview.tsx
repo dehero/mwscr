d8167d60545c24e2ad4699a377c3ddaf7651b991
dehero <dehero@outlook.com>
2025-03-07 15:59:22 +0300

    feat(site): implement file uploading

--- src/site/components/ResourcePreview/ResourcePreview.tsx
+++ src/site/components/ResourcePreview/ResourcePreview.tsx
@@ -1,8 +1,13 @@
 import clsx from 'clsx';
 import type { Component } from 'solid-js';
-import { Match, onMount, Show, Switch } from 'solid-js';
+import {
+  //Match,
+  onMount,
+  Show,
+  // Switch
+} from 'solid-js';
 import type { MediaAspectRatio } from '../../../core/entities/media.js';
-import { parseResourceUrl } from '../../../core/entities/resource.js';
+// import { parseResourceUrl } from '../../../core/entities/resource.js';
 import { getResourcePreviewUrl } from '../../data-managers/resources.js';
 import YellowExclamationMark from '../../images/exclamation.svg';
 import type { FrameState } from '../Frame/Frame.js';
@@ -24,7 +29,7 @@ export interface ResourcePreviewProps {
 
 export const ResourcePreview: Component<ResourcePreviewProps> = (props) => {
   const { addToast } = useToaster();
-  const parsedUrl = () => parseResourceUrl(props.url);
+  // const parsedUrl = () => parseResourceUrl(props.url);
   const url = () => getResourcePreviewUrl(props.url);
   let ref: HTMLImageElement | undefined;
 
@@ -53,37 +58,39 @@ export const ResourcePreview: Component<ResourcePreviewProps> = (props) => {
   return (
     <Show when={url()} keyed>
       {(url) => (
-        <Switch
-          fallback={
-            <Frame
-              variant="thin"
-              class={clsx(styles.fallback, props.class)}
-              style={props.aspectRatio ? { 'aspect-ratio': props.aspectRatio } : undefined}
-              state={props.frameState}
-            >
-              <span class={styles.url}>{props.url}</span>
-            </Frame>
-          }
-        >
-          <Match when={parsedUrl().protocol === 'store:'}>
-            <Frame
-              component="img"
-              src={url}
-              ref={ref}
-              class={clsx(styles.preview, props.class)}
-              draggable="false"
-              onLoad={handleLoad}
-              onError={handleError}
-              style={props.aspectRatio ? { 'aspect-ratio': props.aspectRatio } : undefined}
-              aria-label={url === YellowExclamationMark ? 'yellow exclamation mark' : props.alt || props.url}
-              state={props.frameState}
-            />
+        // <Switch
+        //   fallback={
+        //     <Frame
+        //       variant="thin"
+        //       class={clsx(styles.fallback, props.class)}
+        //       style={props.aspectRatio ? { 'aspect-ratio': props.aspectRatio } : undefined}
+        //       state={props.frameState}
+        //     >
+        //       <span class={styles.url}>{props.url}</span>
+        //     </Frame>
+        //   }
+        // >
+        // {/* <Match when={parsedUrl().protocol === 'store:'}> */}
+        <>
+          <Frame
+            component="img"
+            src={url}
+            ref={ref}
+            class={clsx(styles.preview, props.class)}
+            draggable="false"
+            onLoad={handleLoad}
+            onError={handleError}
+            style={props.aspectRatio ? { 'aspect-ratio': props.aspectRatio } : undefined}
+            aria-label={url === YellowExclamationMark ? 'yellow exclamation mark' : props.alt || props.url}
+            state={props.frameState}
+          />
 
-            <Show when={props.showTooltip}>
-              <Tooltip forRef={ref}>{props.url}</Tooltip>
-            </Show>
-          </Match>
-        </Switch>
+          <Show when={props.showTooltip}>
+            <Tooltip forRef={ref}>{props.url}</Tooltip>
+          </Show>
+        </>
+        // {/* </Match> */}
+        // </Switch>
       )}
     </Show>
   );
