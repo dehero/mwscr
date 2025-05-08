be9dfd3eb93f2f0bef90466edbacd0b70ec512e3
dehero <dehero@outlook.com>
2025-05-08 12:02:29 +0300

    fix: add retries to site store manager

--- src/local/store-managers/site-store-manager.ts
+++ src/local/store-managers/site-store-manager.ts
@@ -6,10 +6,25 @@ import { SiteStore } from '../../core/stores/site-store.js';
 import { debounce } from '../../core/utils/common-utils.js';
 import { streamToBuffer } from '../utils/data-utils.js';
 
+const RETRY_COUNT = 5;
+
 export class SiteStoreManager extends SiteStore implements StoreManager {
   client: Client | undefined;
 
   private waitAndClose = debounce(() => this.client?.close(), 1000);
+
+  private async retry(method: () => Promise<void>): Promise<void> {
+    for (let i = 0; i < RETRY_COUNT; i++) {
+      try {
+        await method();
+        return;
+      } catch (error) {
+        if (i === RETRY_COUNT - 1) {
+          throw error;
+        }
+      }
+    }
+  }
 
   private async connect() {
     const { SITE_FTP_HOST, SITE_FTP_USER, SITE_FTP_PASSWORD, SITE_FTP_STORE_PATH } = process.env;
@@ -45,20 +60,22 @@ export class SiteStoreManager extends SiteStore implements StoreManager {
   }
 
   async copy(from: string, to: string): Promise<void> {
-    try {
-      const { client, path } = await this.connect();
-      const fromPath = posix.join(path, from);
-      const toPath = posix.join(path, to);
+    return this.retry(async () => {
+      try {
+        const { client, path } = await this.connect();
+        const fromPath = posix.join(path, from);
+        const toPath = posix.join(path, to);
 
-      const stream = new Duplex();
+        const stream = new Duplex();
 
-      await client.downloadTo(stream, fromPath);
-      await client.ensureDir(posix.dirname(toPath));
-      await client.cd('/');
-      await client.uploadFrom(stream, toPath);
-    } finally {
-      this.waitAndClose();
-    }
+        await client.downloadTo(stream, fromPath);
+        await client.ensureDir(posix.dirname(toPath));
+        await client.cd('/');
+        await client.uploadFrom(stream, toPath);
+      } finally {
+        this.waitAndClose();
+      }
+    });
   }
 
   async exists(path: string): Promise<boolean> {
@@ -99,40 +116,46 @@ export class SiteStoreManager extends SiteStore implements StoreManager {
   }
 
   async move(from: string, to: string): Promise<void> {
-    try {
-      const { client, path } = await this.connect();
-      const fromPath = posix.join(path, from);
-      const toPath = posix.join(path, to);
+    return this.retry(async () => {
+      try {
+        const { client, path } = await this.connect();
+        const fromPath = posix.join(path, from);
+        const toPath = posix.join(path, to);
 
-      await client.ensureDir(posix.dirname(toPath));
-      await client.cd('/');
-      await client.rename(fromPath, toPath);
-    } finally {
-      this.waitAndClose();
-    }
+        await client.ensureDir(posix.dirname(toPath));
+        await client.cd('/');
+        await client.rename(fromPath, toPath);
+      } finally {
+        this.waitAndClose();
+      }
+    });
   }
 
   async put(path: string, data: Iterable<unknown> | AsyncIterable<unknown>): Promise<void> {
-    const stream = Readable.from(data);
+    return this.retry(async () => {
+      const stream = Readable.from(data);
 
-    try {
-      return this.putStream(path, stream);
-    } finally {
-      this.waitAndClose();
-    }
+      try {
+        return this.putStream(path, stream);
+      } finally {
+        this.waitAndClose();
+      }
+    });
   }
 
   async putStream(path: string, stream: NodeJS.ReadableStream): Promise<void> {
-    try {
-      const site = await this.connect();
-      const filename = posix.join(site.path, path);
+    return this.retry(async () => {
+      try {
+        const site = await this.connect();
+        const filename = posix.join(site.path, path);
 
-      await site.client.ensureDir(posix.dirname(filename));
-      await site.client.cd('/');
-      await site.client.uploadFrom(new Readable().wrap(stream), filename);
-    } finally {
-      this.waitAndClose();
-    }
+        await site.client.ensureDir(posix.dirname(filename));
+        await site.client.cd('/');
+        await site.client.uploadFrom(new Readable().wrap(stream), filename);
+      } finally {
+        this.waitAndClose();
+      }
+    });
   }
 
   async readdir(path: string): Promise<StoreItem[]> {
@@ -151,13 +174,15 @@ export class SiteStoreManager extends SiteStore implements StoreManager {
   }
 
   async remove(path: string): Promise<void> {
-    try {
-      const site = await this.connect();
+    return this.retry(async () => {
+      try {
+        const site = await this.connect();
 
-      await site.client.remove(posix.join(site.path, path));
-    } finally {
-      this.waitAndClose();
-    }
+        await site.client.remove(posix.join(site.path, path));
+      } finally {
+        this.waitAndClose();
+      }
+    });
   }
 }
 
