150716bbf6e54c30c9f70e57b9b50acbe0432a27
dehero <dehero@outlook.com>
2025-05-12 12:50:35 +0300

    test: add resource management tests before maintenance

---
+++ src/local/data-managers/resources.test.ts
@@ -0,0 +1,26 @@
+import assert from 'node:assert';
+import { after, test } from 'node:test';
+import { copyResource, moveResource, removeResource } from './resources.js';
+
+test('copyResource', async (t) => {
+  await t.test('copy external resource to inbox', async () => {
+    after(() => removeResource('store:/inbox/external-resource-copy-test.png'));
+
+    assert.doesNotReject(
+      copyResource(
+        'https://raw.githubusercontent.com/dehero/mwscr/refs/heads/main/assets/icon.png',
+        'store:/inbox/external-resource-copy-test.png',
+      ),
+    );
+  });
+});
+
+test('moveResource', async (t) => {
+  await t.test('move file from inbox to shots', async () => {
+    after(() => removeResource('store:/shots/move-resource-test.png'));
+
+    await copyResource('store:/shots/2016-11-20-1-dren-plantation.png', 'store:/inbox/move-resource-test.png');
+
+    assert.doesNotReject(moveResource('store:/inbox/move-resource-test.png', 'store:/shots/move-resource-test.png'));
+  });
+});
