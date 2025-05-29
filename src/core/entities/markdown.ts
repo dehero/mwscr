3216d166870179f92b2d5fd5225151afc5522528
dehero <dehero@outlook.com>
2025-05-29 10:35:13 +0300

    feat: implement rendering post stories

--- src/core/entities/markdown.ts
+++ src/core/entities/markdown.ts
@@ -1,18 +1,25 @@
+const MARKDOWN_LINK_LINE_REGEX = /^[^\S\r\n]*\[([^\]]+)\]\(([^)]+)\)[^\S\r\n]*$/gm;
 const MARKDOWN_LINK_REGEX = /\[([^\]]+)\]\(([^)]+)\)/gm;
 const MARKDOWN_DOUBLE_BR_REGEX = /(?:\r?\n){2}/gm;
 const MARKDOWN_BR_REGEX = /\s\s$/gm;
+const MARKDOWN_HTML_BR_REGEX = /<br\s*\/?>/gm;
 const MARKDOWN_TITLE_REGEX = /#\s*(.*)/m;
 
 export type MarkdownLinkDescriptor = [href?: string, external?: boolean];
 
 export type MarkdownLinkReplacer = (url: string) => MarkdownLinkDescriptor;
 
-export function markdownToHtml(markdown: string, linkReplacer?: MarkdownLinkReplacer) {
+export function markdownToInlineHtml(markdown: string, linkReplacer?: MarkdownLinkReplacer) {
   let title;
   const html = markdown
     .replace(MARKDOWN_TITLE_REGEX, (_, match) => {
       title = match;
       return '';
+    })
+    .replaceAll(MARKDOWN_LINK_LINE_REGEX, (_, title, url) => {
+      const [href, external] = linkReplacer?.(url) ?? [url];
+
+      return href ? `<a href="${href}"${external ? ' target="_blank"' : ''}>${title}</a>  ` : '';
     })
     .replaceAll(MARKDOWN_LINK_REGEX, (_, title, url) => {
       const [href, external] = linkReplacer?.(url) ?? [url];
@@ -24,4 +31,36 @@ export function markdownToHtml(markdown: string, linkReplacer?: MarkdownLinkRepl
     .replaceAll(MARKDOWN_BR_REGEX, '<br />');
 
   return { html, title };
+}
+
+export function markdownToText(markdown: string, appendlinks?: boolean) {
+  let title;
+  const links: string[] = [];
+
+  let text = markdown
+    .replaceAll(MARKDOWN_HTML_BR_REGEX, '  \n')
+    .replace(MARKDOWN_TITLE_REGEX, (_, match) => {
+      title = match;
+      return '';
+    })
+    .replaceAll(MARKDOWN_LINK_LINE_REGEX, (_, title, url) => {
+      if (url) {
+        links.push(`${title}: ${url}`);
+        return '';
+      }
+      return title;
+    })
+    .replaceAll(MARKDOWN_LINK_REGEX, (_, title, url) => {
+      links.push(url);
+      return title;
+    })
+    .trim()
+    .replaceAll(MARKDOWN_DOUBLE_BR_REGEX, '\n\n')
+    .replaceAll(MARKDOWN_BR_REGEX, '\n');
+
+  if (appendlinks && links.length > 0) {
+    text += '\n\n' + links.join('\n');
+  }
+
+  return { text, title, links };
 }
