039cc97e608f629943db6da39d60a6a01f1a044c
dehero <dehero@outlook.com>
2025-06-02 15:46:39 +0300

    fix: add more puppeteer args to make it work in GitHub Actions

--- src/local/utils/image-utils.ts
+++ src/local/utils/image-utils.ts
@@ -1,7 +1,12 @@
 import nodeHtmlToImage from 'node-html-to-image';
 
 export async function htmlToImage(html: string): Promise<Buffer> {
-  const result = await nodeHtmlToImage({ html, puppeteerArgs: { args: ['--no-sandbox'] } });
+  const result = await nodeHtmlToImage({
+    html,
+    puppeteerArgs: {
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu', '--headless'],
+    },
+  });
 
   if (!Buffer.isBuffer(result)) {
     throw new TypeError('Failed to create story image.');
