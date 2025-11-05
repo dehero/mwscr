6e83ae901eec7c15472dfbd381fb43d09b443895
dehero <dehero@outlook.com>
2025-11-05 09:05:59 +0300

    chore: replace flip calendar month images

--- src/scripts/create-calendar.ts
+++ src/scripts/create-calendar.ts
@@ -5,17 +5,17 @@ await renderCalendarToFolder({
   year: 2026,
   ru: true,
   images: [
-    'store:/shots/2018-11-09-dusk-in-the-skaal-village.png',
-    'store:/shots/2018-11-11-through-the-snowy-forest.png',
-    'store:/shots/2016-12-28-close-to-caldera-clothier.png',
-    'store:/shots/2024-12-30-cold-rain-of-bitter-coast.png',
-    'store:/shots/2017-03-16-in-the-chambers-of-nchurdamz.png',
-    'store:/shots/2017-04-26-divine-pentagon.png',
-    'store:/shots/2025-04-25-st-delyn-and-masser.png',
-    'store:/shots/2018-08-20-mushroom-glade.png',
-    'store:/shots/2018-01-09-dead-trees-of-the-east-coast.png',
-    'store:/shots/2018-09-22-two-ways.png',
-    'store:/shots/2017-01-06-daedroth-in-grazelands.png',
-    'store:/shots/2018-02-21-view-from-the-coastal-rocks.png',
+    'store:/shots/2018-02-21-view-from-the-coastal-rocks.png', // Месяц Утренней Звезды
+    'store:/shots/2018-01-09-dead-trees-of-the-east-coast.png', // Месяц Восхода
+    'store:/shots/2016-12-20-a-boxy-nightstand.png', // Месяц Первоцвета
+    'store:/shots/2024-12-30-cold-rain-of-bitter-coast.png', // Месяц Дождя
+    'store:/shots/2016-12-28-close-to-caldera-clothier.png', // Месяц Сева
+    'store:/shots/2018-09-22-two-ways.png', // Месяц Середины Года
+    'store:/shots/2018-01-27-evening-beat.png', // Месяц Солнцеворота
+    'store:/shots/2018-08-20-mushroom-glade.png', // Месяц Урожая
+    'store:/shots/2017-04-26-divine-pentagon.png', // Месяц Огня
+    'store:/shots/2018-06-29-road-to-a-dwemer-tower.png', // Месяц Мороза
+    'store:/shots/2017-01-06-daedroth-in-grazelands.png', // Месяц Заката
+    'store:/shots/2018-11-09-dusk-in-the-skaal-village.png', // Месяц Вечерней Звезды
   ],
 });
