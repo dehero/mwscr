6adcb19af3efcf7fcb31a8f80f6c677cd2a7ee5c
dehero <dehero@outlook.com>
2025-11-26 11:00:00 +0300

    feat: add class property to Markdown control

--- src/site/components/Markdown/Markdown.tsx
+++ src/site/components/Markdown/Markdown.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import type { Component } from 'solid-js';
 import { markdownToInlineHtml } from '../../../core/entities/markdown.js';
 import styles from './Markdown.module.css';
@@ -5,10 +6,11 @@ import styles from './Markdown.module.css';
 interface MarkdownProps {
   children: string;
   disableLinks?: boolean;
+  class?: string;
 }
 
 export const Markdown: Component<MarkdownProps> = (props) => {
   const html = () => markdownToInlineHtml(props.children, props.disableLinks ? () => [] : undefined).html;
 
-  return <p class={styles.markdown} innerHTML={html()} />;
+  return <p class={clsx(styles.markdown, props.class)} innerHTML={html()} />;
 };
