b5268664b76516e35627b0fdb9888c81195ebc9a
dehero <auto@dehero.site>
2026-03-05 14:42:22 +0300

    feat: remove separate GitHub issues for post editing and locating

--- src/core/github-issues/index.ts
+++ src/core/github-issues/index.ts
@@ -1,8 +1,6 @@
 import type { GithubIssueInfo } from '../entities/github-issue.js';
 import * as dataPatch from './data-patch.js';
-import * as postEditing from './post-editing.js';
-import * as postLocation from './post-location.js';
 import * as postProposal from './post-proposal.js';
 import * as postRequest from './post-request.js';
 
-export const githubIssues: GithubIssueInfo[] = [dataPatch, postProposal, postRequest, postEditing, postLocation];
+export const githubIssues: GithubIssueInfo[] = [dataPatch, postProposal, postRequest];
--- src/scripts/github-issue-resolvers/index.ts
+++ src/scripts/github-issue-resolvers/index.ts
@@ -1,14 +1,6 @@
 import type { GithubIssueResolver } from '../../core/entities/github-issue.js';
 import * as dataPatch from './data-patch.js';
-import * as postEditing from './post-editing.js';
-import * as postLocation from './post-location.js';
 import * as postProposal from './post-proposal.js';
 import * as postRequest from './post-request.js';
 
-export const githubIssueResolvers: GithubIssueResolver[] = [
-  dataPatch,
-  postProposal,
-  postRequest,
-  postEditing,
-  postLocation,
-];
+export const githubIssueResolvers: GithubIssueResolver[] = [dataPatch, postProposal, postRequest];
