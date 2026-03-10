aefed5c9e4a320f2ba979eb123c6bd89205f1460
dehero <auto@dehero.site>
2026-03-10 09:07:28 +0300

    fix(site): use inner scroll for contributing variants list

--- src/site/components/ContributingDialog/ContributingDialog.tsx
+++ src/site/components/ContributingDialog/ContributingDialog.tsx
@@ -19,104 +19,106 @@ export const ContributingDialog: DetachedDialog<ContributingDialogTab> = (props)
   const tab = () => props.pathname ?? 'variants';
   const patchSize = useLocalPatch();
 
+  let creatingCompilationRef;
+
   return (
     <Dialog
       title="Contributing"
       modal
       {...props}
       actions={[<Button onClick={props.onClose}>OK</Button>]}
+      contentClass={styles.container}
       summary={
         <a href="https://github.com/dehero/mwscr/blob/main/CONTRIBUTING.md" class={styles.link} target="_blank">
           Guidelines
         </a>
       }
     >
-      <div class={styles.container}>
-        <div class={styles.tabs}>
-          <Button active={tab() === 'variants'} href={createDetachedDialogFragment('contributing', 'variants')}>
-            Variants
-          </Button>
-          <Button active={tab() === 'patch'} href={createDetachedDialogFragment('contributing', 'patch')}>
-            Edits {patchSize() > 0 ? ` (${patchSize()})` : ''}
-          </Button>
-        </div>
-
-        <Frame class={styles.tabContent}>
-          <Switch>
-            <Match when={tab() === 'variants'}>
-              <div class={styles.variants}>
-                <Frame
-                  component="a"
-                  href={createDetachedDialogFragment('post-proposal')}
-                  onClick={props.onClose}
-                  variant="thin"
-                  class={styles.variant}
-                >
-                  <Icon color="stealth" class={styles.variantIcon}>
-                    S
-                  </Icon>
-                  <p class={styles.variantTitle}>Submit Files</p>
-                  <p class={styles.variantDescription}>Add your images or videos to Drafts.</p>
-                </Frame>
-
-                <Frame
-                  component="a"
-                  href={
-                    postsRoute.createUrl({ managerName: 'posts', type: 'shot', original: 'true' }) +
-                    createDetachedDialogFragment('topic', 'creating-compilation')
-                  }
-                  onClick={props.onClose}
-                  variant="thin"
-                  class={styles.variant}
-                >
-                  <Icon color="stealth" class={styles.variantIcon}>
-                    C
-                  </Icon>
-                  <p class={styles.variantTitle}>Create Compilation</p>
-                  <p class={styles.variantDescription}>Combine published shots into your own compilation.</p>
-                </Frame>
-
-                <Frame
-                  component="a"
-                  href={createDetachedDialogFragment('post-request', 'drafts')}
-                  onClick={props.onClose}
-                  variant="thin"
-                  class={styles.variant}
-                >
-                  <Icon color="magic" class={styles.variantIcon}>
-                    R
-                  </Icon>
-                  <p class={styles.variantTitle}>Request Themed Post</p>
-                  <p class={styles.variantDescription}>Ask the authors to make a certain post.</p>
-                </Frame>
-
-                <Frame
-                  component="a"
-                  href={
-                    postsRoute.createUrl({ managerName: 'posts', location: NONE_OPTION.value, original: 'true' }) +
-                    createDetachedDialogFragment('topic', 'suggesting-location')
-                  }
-                  onClick={props.onClose}
-                  variant="thin"
-                  class={styles.variant}
-                >
-                  <Icon color="magic" class={styles.variantIcon}>
-                    L
-                  </Icon>
-                  <p class={styles.variantTitle}>Find Missing Location</p>
-                  <p class={styles.variantDescription}>
-                    Suggest shooting location of screenshot or video if not specified in the post.
-                  </p>
-                </Frame>
-              </div>
-            </Match>
-
-            <Match when={tab() === 'patch'}>
-              <DataPatchEditor class={styles.patchEditor} />
-            </Match>
-          </Switch>
-        </Frame>
+      <div class={styles.tabs}>
+        <Button active={tab() === 'variants'} href={createDetachedDialogFragment('contributing', 'variants')}>
+          Variants
+        </Button>
+        <Button active={tab() === 'patch'} href={createDetachedDialogFragment('contributing', 'patch')}>
+          Edits {patchSize() > 0 ? ` (${patchSize()})` : ''}
+        </Button>
       </div>
+
+      <Frame class={styles.tabContent}>
+        <Switch>
+          <Match when={tab() === 'variants'}>
+            <div class={styles.variants}>
+              <Frame
+                component="a"
+                href={createDetachedDialogFragment('post-proposal')}
+                onClick={props.onClose}
+                variant="thin"
+                class={styles.variant}
+              >
+                <Icon color="stealth" class={styles.variantIcon}>
+                  S
+                </Icon>
+                <p class={styles.variantTitle}>Submit Files</p>
+                <p class={styles.variantDescription}>Add your images or videos to Drafts.</p>
+              </Frame>
+
+              <Frame
+                component="a"
+                href={
+                  postsRoute.createUrl({ managerName: 'posts', type: 'shot', original: 'true' }) +
+                  createDetachedDialogFragment('topic', 'creating-compilation')
+                }
+                onClick={props.onClose}
+                variant="thin"
+                class={styles.variant}
+                ref={creatingCompilationRef}
+              >
+                <Icon color="stealth" class={styles.variantIcon}>
+                  C
+                </Icon>
+                <p class={styles.variantTitle}>Create Compilation</p>
+                <p class={styles.variantDescription}>Combine published shots into your own compilation.</p>
+              </Frame>
+
+              <Frame
+                component="a"
+                href={createDetachedDialogFragment('post-request', 'drafts')}
+                onClick={props.onClose}
+                variant="thin"
+                class={styles.variant}
+              >
+                <Icon color="magic" class={styles.variantIcon}>
+                  R
+                </Icon>
+                <p class={styles.variantTitle}>Request Themed Post</p>
+                <p class={styles.variantDescription}>Ask the authors to make a certain post.</p>
+              </Frame>
+
+              <Frame
+                component="a"
+                href={
+                  postsRoute.createUrl({ managerName: 'posts', location: NONE_OPTION.value, original: 'true' }) +
+                  createDetachedDialogFragment('topic', 'suggesting-location')
+                }
+                onClick={props.onClose}
+                variant="thin"
+                class={styles.variant}
+              >
+                <Icon color="magic" class={styles.variantIcon}>
+                  L
+                </Icon>
+                <p class={styles.variantTitle}>Find Missing Location</p>
+                <p class={styles.variantDescription}>
+                  Suggest shooting location of screenshot or video if not specified in the post.
+                </p>
+              </Frame>
+            </div>
+          </Match>
+
+          <Match when={tab() === 'patch'}>
+            <DataPatchEditor class={styles.patchEditor} />
+          </Match>
+        </Switch>
+      </Frame>
     </Dialog>
   );
 };
