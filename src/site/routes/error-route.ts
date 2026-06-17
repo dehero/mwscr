851c9997895b678aab2a68d6a6efc5176d748f7d
dehero <auto@dehero.site>
2026-06-17 15:08:30 +0300

    fix(site): align error and image editor routes

--- src/site/routes/error-route.ts
+++ src/site/routes/error-route.ts
@@ -1,9 +1,14 @@
 import type { SiteRoute } from '../../core/entities/site-route.js';
 import { ErrorPage } from '../pages/ErrorPage/ErrorPage.jsx';
+import { homeRoute } from './home-route.js';
 
 export const errorRoute: SiteRoute = {
   path: '*',
   info: () => ({}),
+  parent: () => ({
+    route: homeRoute,
+    params: {},
+  }),
   createUrl: () => '/error/',
   component: ErrorPage,
 };
--- src/site/routes/image-editor-route.ts
+++ src/site/routes/image-editor-route.ts
@@ -1,11 +1,16 @@
 import { lazy } from 'solid-js';
 import type { SiteRoute } from '../../core/entities/site-route.js';
+import { homeRoute } from './home-route.js';
 
 export const imageEditorRoute: SiteRoute = {
   path: '/image-editor',
   info: () => ({
     label: 'Image Editor',
   }),
-  createUrl: () => '/image-editor',
+  parent: () => ({
+    route: homeRoute,
+    params: {},
+  }),
+  createUrl: () => '/image-editor/',
   component: lazy(() => import('../pages/ImageEditorPage/ImageEditorPage.jsx')),
 };
